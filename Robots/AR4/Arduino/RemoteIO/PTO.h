4a7d5dbb3447badb62aa3df5c245dd54fdf33af0
Dale Thomas <dalethomas81@gmail.com>
2026-09-13 13:55:05 -0700

    update PTO in remote IO

--- Robots/AR4/Arduino/RemoteIO/PTO.h
+++ Robots/AR4/Arduino/RemoteIO/PTO.h
@@ -5,24 +5,35 @@
 #include <Arduino.h>
 #include <digitalWriteFast.h>
 
-#define THROW_ERROR_IF_NOT_FAST 
+#define THROW_ERROR_IF_NOT_FAST
+
+// Max half-period edges to emit per run() call. Limits blocking if the
+// main loop falls far behind a high commanded frequency.
+#ifndef PTO_MAX_EDGES_PER_CALL
+#define PTO_MAX_EDGES_PER_CALL 8
+#endif
 
 class PTO {
   private:
-    bool Direction, Direction_Last;
-    uint32_t _SysTime, _SysTime_Last;
-    int dirPin;
-    int pulsePin;
-    int enablePin;
-    bool bOffOneshot;
+    bool _direction;              // true = HIGH (negative frequency)
+    uint32_t _sysTimeLast;        // cycle count at last edge (phase reference)
+    uint32_t _halfPeriodCycles;   // CPU cycles per half-period (50% duty)
+    int16_t _frequencyLast;       // last applied frequency (0 = stopped)
+    int _dirPin;
+    int _pulsePin;
+    int _enablePin;
+    bool _offOneshot;
+    bool _pulseHigh;              // tracks step pin state for DIR setup
+
+    void applyFrequency(int16_t frequency);
 
   public:
-    PTO(int In_DirPin, int In_PulsePin, int In_EnablePin = -1);
+    PTO(int dirPin, int pulsePin, int enablePin = -1);
     void init();
-    void run(int16_t Frequency);
-    int turnON();
-    int turnOFF();
-    bool DriveEnabled;
-};
+    void run(int16_t frequency);
+    int enable();
+    int disable();
+    bool enabled;
 
+};
 #endif
